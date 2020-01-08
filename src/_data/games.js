ff0e2d15c55093e4c2da2163de0677e7372e265d
Pixelastic <tim@pixelastic.com>
2020-01-08 17:13:52 +0100

    Added all games

--- src/_data/games.js
+++ src/_data/games.js
@@ -1,9 +1,37 @@
 import poe from '@pixelastic/videogames-assets-poe';
+import commandos from '@pixelastic/videogames-assets-commandos';
+import warhammerquest from '@pixelastic/videogames-assets-warhammerquest';
+import tombraider from '@pixelastic/videogames-assets-tombraider';
+import tinybrains from '@pixelastic/videogames-assets-tinybrains';
+import thief from '@pixelastic/videogames-assets-thief';
+import spelunky from '@pixelastic/videogames-assets-spelunky';
+import skyrim from '@pixelastic/videogames-assets-skyrim';
+import misc from '@pixelastic/videogames-assets-misc';
+import legoHarryPotterYear57 from '@pixelastic/videogames-assets-lego-harry-potter-year-5-7';
+import dishonored from '@pixelastic/videogames-assets-dishonored';
+import castlecrashers from '@pixelastic/videogames-assets-castlecrashers';
+import gta5 from '@pixelastic/videogames-assets-gta5';
+import hearthstone from '@pixelastic/videogames-assets-hearthstone';
+// {IMPORT}
 
 export default [
+  { name: 'Pillars of Eternity', slug: 'poe', manifest: poe },
+  { name: 'Commandos', slug: 'commandos', manifest: commandos },
+  { name: 'Warhammer Quest', slug: 'warhammerquest', manifest: warhammerquest },
+  { name: 'Tombraider', slug: 'tombraider', manifest: tombraider },
+  { name: 'Tinybrains', slug: 'tinybrains', manifest: tinybrains },
+  { name: 'Thief', slug: 'thief', manifest: thief },
+  { name: 'Spelunky', slug: 'spelunky', manifest: spelunky },
+  { name: 'Skyrim', slug: 'skyrim', manifest: skyrim },
+  { name: 'Misc', slug: 'misc', manifest: misc },
   {
-    name: 'Pillars of Eternity',
-    slug: 'poe',
-    manifest: poe,
+    name: 'Lego Harry Potter Years 5-7',
+    slug: 'lego-harry-potter-year-5-7',
+    manifest: legoHarryPotterYear57,
   },
+  { name: 'Dishonored', slug: 'dishonored', manifest: dishonored },
+  { name: 'Castlecrashers', slug: 'castlecrashers', manifest: castlecrashers },
+  { name: 'Gta 5', slug: 'gta5', manifest: gta5 },
+  { name: 'Hearthstone', slug: 'hearthstone', manifest: hearthstone },
+  // {CONFIG}}
 ];
