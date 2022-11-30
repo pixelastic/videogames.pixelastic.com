bb3b9b66a961d6df6c63d196b51cd0cc906f88fe
Tim Carry <tim@pixelastic.com>
2022-12-01 00:07:17 +0100

    Add gta3

--- src/_data/games.js
+++ src/_data/games.js
@@ -2,6 +2,7 @@ const bgeet = require('@pixelastic/videogames-assets-bgeet');
 const castlecrashers = require('@pixelastic/videogames-assets-castlecrashers');
 const commandos = require('@pixelastic/videogames-assets-commandos');
 const dishonored = require('@pixelastic/videogames-assets-dishonored');
+const gta3 = require('@pixelastic/videogames-assets-gta3');
 const gta5 = require('@pixelastic/videogames-assets-gta5');
 const hearthstone = require('@pixelastic/videogames-assets-hearthstone');
 const legoHarryPotterYear57 = require('@pixelastic/videogames-assets-lego-harry-potter-year-5-7');
@@ -9,13 +10,14 @@ const misc = require('@pixelastic/videogames-assets-misc');
 const poe = require('@pixelastic/videogames-assets-poe');
 const skyrim = require('@pixelastic/videogames-assets-skyrim');
 const spelunky = require('@pixelastic/videogames-assets-spelunky');
+const talosPrinciple = require('@pixelastic/videogames-assets-talos-principle');
 const thief = require('@pixelastic/videogames-assets-thief');
+const thronebreaker = require('@pixelastic/videogames-assets-thronebreaker');
 const tinybrains = require('@pixelastic/videogames-assets-tinybrains');
 const tombraider = require('@pixelastic/videogames-assets-tombraider');
 const warhammerquest = require('@pixelastic/videogames-assets-warhammerquest');
-const talosPrinciple = require('@pixelastic/videogames-assets-talos-principle');
-const thronebreaker = require('@pixelastic/videogames-assets-thronebreaker');
 module.exports = [
+  { name: 'GTA 3', slug: 'gta3', manifest: gta3 },
   {
     name: "Baldur's Gate Enhanced Edition Trilogy",
     slug: 'bgeet',
@@ -59,6 +61,6 @@ module.exports = [
     slug: 'castlecrashers',
     manifest: castlecrashers,
   },
-  { name: 'Gta 5', slug: 'gta5', manifest: gta5 },
+  { name: 'GTA 5', slug: 'gta5', manifest: gta5 },
   { name: 'Hearthstone', slug: 'hearthstone', manifest: hearthstone },
 ];
