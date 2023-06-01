11e32eca180eb6f7fdbf5345ab4aa157a0ec1f9d
Tim Carry <tim@pixelastic.com>
2023-06-02 00:19:43 +0200

    Add Botw

--- src/_data/games.js
+++ src/_data/games.js
@@ -1,5 +1,6 @@
-const bgeet = require('@pixelastic/videogames-assets-bgeet');
 const batmanArkhamCity = require('@pixelastic/videogames-assets-batman-arkham-city');
+const bgeet = require('@pixelastic/videogames-assets-bgeet');
+const breathOfTheWild = require('@pixelastic/videogames-assets-breath-of-the-wild');
 const castlecrashers = require('@pixelastic/videogames-assets-castlecrashers');
 const commandos = require('@pixelastic/videogames-assets-commandos');
 const dishonored = require('@pixelastic/videogames-assets-dishonored');
@@ -8,8 +9,8 @@ const gta5 = require('@pixelastic/videogames-assets-gta5');
 const hearthstone = require('@pixelastic/videogames-assets-hearthstone');
 const legoHarryPotterYear57 = require('@pixelastic/videogames-assets-lego-harry-potter-year-5-7');
 const misc = require('@pixelastic/videogames-assets-misc');
-const poe = require('@pixelastic/videogames-assets-poe');
 const poe2 = require('@pixelastic/videogames-assets-poe2');
+const poe = require('@pixelastic/videogames-assets-poe');
 const skyrim = require('@pixelastic/videogames-assets-skyrim');
 const spelunky = require('@pixelastic/videogames-assets-spelunky');
 const superMarioOdyssey = require('@pixelastic/videogames-assets-super-mario-odyssey');
@@ -38,6 +39,12 @@ module.exports = [
     date: '2021-01-17',
     slug: 'thronebreaker',
     manifest: thronebreaker,
+  },
+  {
+    name: 'Breath of the Wild',
+    date: '2017-12-25',
+    slug: 'botw',
+    manifest: breathOfTheWild,
   },
   {
     name: 'Spelunky',
